b9a1164f3b5defb69e1b2624b03a4eda1c0fdf9e
Surena Golijani <surena.golijani@cabag.ch>
2021-03-12 11:17:26 +0100

    [TASK] Add condition to set FE_alwaysFetchUser

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
@@ -9,8 +12,12 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['acti
 // Hook to check for redirection
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'Cabag\CabagLoginas\Hook\PostUserLookupHook->postUserLookUp';
 
-// Trigger authentication even if login is not requested explicitely
-$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_alwaysFetchUser'] = 1;
+// Needed to trigger authentication without setting FE_alwaysFetchUser globally
+$cabag_loginas_data = GeneralUtility::_GP('tx_cabagloginas');
+if (isset($cabag_loginas_data['userid']) && ($cabag_loginas_data['userid'] > 0)) {
+    // Trigger authentication without setting FE_alwaysFetchUser globally
+    $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_alwaysFetchUser'] = true;
+}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Cabag\\CabagLoginas\\Service\\LoginAsService' /* sv key */,
 	array(
